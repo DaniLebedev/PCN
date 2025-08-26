9204b544eb05913f307f57153da41c1f402b06b7
DaniLebedev <danil.lebedev08@mail.ru>
2025-08-26 19:02:01 +0500

    stable edition

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
-import HeroImg from "../assets/images/hero-img.png";
+import HeroImg from "../assets/images/background.jpg";
+import HeroMan from "../assets/images/hero-men.gif";
 const Home: React.FC = () => {
     return (
         <main className="home-page">
@@ -8,15 +9,17 @@ const Home: React.FC = () => {
                 <div className="hero-block">
                     <div className="img-hero-block">
                         <img src={HeroImg} alt="" />
-                        <div className="dark-hero-block"></div>
                     </div>
+                    <div className="dark-hero-block"></div>
                     <div className="hero-container-text">
-                        <h1 className="main-hero-block">
+                        <div className="container">
+                            <h1 className="main-hero-block">
                             PCN-101
                         </h1>
                         <h1 className="secondary-hero-block">
                             PCN-101
                         </h1>
+                        </div>
                     </div>
                 </div>
             </section>
